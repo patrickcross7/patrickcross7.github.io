3975f29226a8ddd27752a6da12feefe5969b83e8
Patrick <patrickcross9@gmail.com>
2023-07-30 18:57:21 -0400

    Organizing and fixing bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Student', 'Software Engineer','Data Scientist', 'Software Developer', , 'Lifelong Learner', 'Creative Problem Solver', 'Tech Enthusiast', 'Team Player', 'Passionate Coder', 
+    strings: ['Student', 'Software Engineer','Data Scientist', 'Software Developer', 'Lifelong Learner', 'Creative Problem Solver', 'Tech Enthusiast', 'Team Player', 'Passionate Coder', 
 'Innovator', 'Sports Addict', 'Fitness Fanatic '],
     typeSpeed: 100,
     backSpeed: 100, 
