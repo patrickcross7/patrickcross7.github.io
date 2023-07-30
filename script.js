0380ed6d00be6524cafa86d8ac754b16acee96b8
Patrick <patrickcross9@gmail.com>
2023-07-30 18:53:02 -0400

    Changed some of the highlighted words

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // typed js
-
 const typed = new Typed('.multiple-text', {
-    strings: ['Student', 'Software Engineer', 'Software Developer','Data Scientist', 'Passionate Coder', 'Lifelong Learner', 'Creative Problem Solver', 'Tech Enthusiast', 'Team Player', 
+    strings: ['Student', 'Software Engineer','Data Scientist', 'Software Developer', , 'Lifelong Learner', 'Creative Problem Solver', 'Tech Enthusiast', 'Team Player', 'Passionate Coder', 
 'Innovator', 'Sports Addict', 'Fitness Fanatic '],
     typeSpeed: 100,
     backSpeed: 100, 
