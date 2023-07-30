d436635aefa73991d9d1af7e17d153bdc61f684f
Patrick <patrickcross9@gmail.com>
2023-07-30 11:09:07 -0400

    Added a typed js class into the website

---
+++ script.js
@@ -0,0 +1,10 @@
+// typed js
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Student', 'Software Engineer', 'Software Developer','Data Scientist', 'Passionate Coder', 'Lifelong Learner', 'Creative Problem Solver', 'Tech Enthusiast', 'Team Player', 
+'Innovator', 'Sports Addict', 'Fitness Fanatic '],
+    typeSpeed: 100,
+    backSpeed: 100, 
+    backDelay: 1000, 
+    loop: true
+});
