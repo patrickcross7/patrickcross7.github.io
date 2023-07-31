5f79d94e1757d596a8d4784d96d78457cfcc1f9a
Patrick <patrickcross9@gmail.com>
2023-07-30 20:34:03 -0400

    messed with the speed of the typing feature

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Student', 'Software Engineer','Data Scientist', 'Software Developer', 'Lifelong Learner', 'Creative Problem Solver', 'Tech Enthusiast', 'Team Player', 'Passionate Coder', 
-'Innovator', 'Sports Addict', 'Fitness Fanatic '],
-    typeSpeed: 100,
-    backSpeed: 100, 
-    backDelay: 1000, 
+    strings: ['Student', 
+    'Software Engineer',
+    'Data Scientist', 
+    'Software Developer', 
+    'Lifelong Learner', 
+    'Creative Problem Solver', 
+    'Tech Enthusiast', 
+    'Team Player', 
+    'Passionate Coder', 
+    'Innovator', 
+    'Sports Addict', 
+    'Fitness Fanatic '],
+    typeSpeed: 75,
+    backSpeed: 75, 
+    backDelay: 2000, 
     loop: true
 });
